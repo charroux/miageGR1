8fb0e12d1a83475a9cbd66e3b203a75dcce55618
charroux <benoit.charroux@gmail.com>
2024-10-25 13:52:18 +0200

    newcarservice

--- src/main/java/com/example/miageGR1/web/CarRental.java
+++ src/main/java/com/example/miageGR1/web/CarRental.java
@@ -16,7 +16,6 @@ public class CarRental {
     public CarRental() {
         cars.add(new Car("11AA22", 1000));
         cars.add(new Car("33CCDD", 3000));
-        cars.add(new Car("22BB33", 2000));
     }
 
     @GetMapping("/cars")
